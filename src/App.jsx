2667c3742e3239f1f40a6ca1cd1e181e0890f853
Phakphoom Sukkhachat <prefix.2pm@gmail.com>
2025-01-04 19:01:11 +0700

    feat: :sparkles: add profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import EditProfile from "./components/EditProfile";
+import Profile from "./components/Profile";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
         <Routes>
           <Route>
             <Route path="/" element={<Dashboard />} />
+            <Route path="/profile" element={<Profile />} />
             <Route path="/edit_profile" element={<EditProfile />} />
           </Route>
         </Routes>
